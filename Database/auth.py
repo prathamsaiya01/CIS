25e351652d918b161e2f378c94cacb4e4dc1c9ee
prathamsaiya01 <prathamsaiya01@gmail.com>
2026-03-23 21:32:00 +0530

    Updated auth and db logic

--- Database/auth.py
+++ Database/auth.py
@@ -1,3 +1,25 @@
+<<<<<<< HEAD
+=======
+from database.db import connect_db
+
+def signup(username, password, avatar):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(
+            "INSERT INTO users (username, password, avatar) VALUES (?, ?, ?)",
+            (username, password, avatar)
+        )
+        conn.commit()
+        return True
+    except:
+        return False
+    finally:
+        conn.close()
+
+
+>>>>>>> fb7855b8f66b1a487003ea679eeffb4acf2fc5e6
 def login(username, password):
     conn = connect_db()
     cursor = conn.cursor()
